ec68f7088c4b5e0d640184b3097a20b96c0a5eb7
notplants <max@mfowler.info>
2022-05-16 14:07:20 +0200

    Fix multiple services

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,6 +13,22 @@ description="Secure Scuttlebutt Pub Admin Interface"
 # PERSONAL HELPERS
 #=================================================
 
+start_systemd_services () {
+    # the first argument of this function $1, is $app
+    ynh_systemd_action --service_name="$1-go-sbot.service" --action="start" --log_path="/var/log/$1/$1.log"
+    ynh_systemd_action --service_name="$1-peach-web.service" --action="start" --log_path="/var/log/$1/$1.log"
+
+    # restart peach-web (this is hack, but it avoids an unexplained 502 error)
+    ynh_systemd_action --service_name="$1-peach-web.service" --action="restart" --log_path="/var/log/$1/$1.log"
+}
+
+stop_systemd_services () {
+    # the first argument of this function $1, is $app
+    ynh_systemd_action --service_name=$app-go-sbot.service --action="stop" --log_path="/var/log/$app/$app.log"
+    ynh_systemd_action --service_name=$app-peach-web.service --action="stop" --log_path="/var/log/$app/$app.log"
+
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
@@ -20,3 +36,4 @@ description="Secure Scuttlebutt Pub Admin Interface"
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
