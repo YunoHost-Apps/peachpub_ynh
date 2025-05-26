7ad167f3588031809689f8429c4f11f73d8199b5
notplants <mfowler.email@protonmail.com>
2025-05-26 17:55:30 -0400

    add password config to install script

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-yunohost app install /srv/peachpub_ynh --args "domain=bpub.commoninternet.net&path=/&language=en&admin=np&password=peachcloud&init_main_permission=visitors" -f
+yunohost app install /srv/peachpub_ynh --args "domain=bpub.commoninternet.net&path=/&language=en&admin=np&password=peachcloud3&init_main_permission=visitors" -f
